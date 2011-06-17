d5380ea91ea482f20110d9e69700a0dd81f21509
Alexander.Chukov@gmail.com <Alexander.Chukov@gmail.com@09c6a7a9-7299-458a-8abf-67d8899b52e1>
2011-06-17 15:05:29 +0000

    catch input byte from tape ROM routine and tape menu

--- trunk/tui.c
+++ trunk/tui.c
@@ -2,20 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "i8080.h"
-//#include "tui.h"
-
-typedef struct {
-    char	*text;
-    void	*next;
-} menu_item;
-
-typedef struct {
-    uns16	w;
-    uns16	h;
-    uns16	vis_start;
-    uns16	cur_pos;
-    menu_item	*item;
-} tui_menu;
+#include "tui.h"
 
 void draw_char(uns16 x, uns16 y, uns8 c);
 
@@ -43,6 +30,7 @@ tui_menu *tui_menu_new(uns16 w, uns16 h)
     tmp->h = h;
     tmp->vis_start = 0;
     tmp->cur_pos = 0;
+    tmp->num_items = 0;
     tmp->item = NULL;
 
     return tmp;
@@ -65,6 +53,8 @@ int tui_menu_add_item(tui_menu *m, char *text)
 	    item = item->next;
 	item->next = tmp;
     }
+
+    m->num_items++;
 
     return 0;
 }
@@ -90,7 +80,7 @@ void tui_menu_dump(tui_menu *m)
     }
 }
 
-void tui_menu_draw(uns16 x, uns16 y, tui_menu *m)
+void tui_menu_draw(tui_menu *m, uns16 x, uns16 y)
 {
     int i, j;
     int w = m->w;
@@ -100,7 +90,6 @@ void tui_menu_draw(uns16 x, uns16 y, tui_menu *m)
     draw_box(x, y, w, h, 'X');
     draw_box(x + 1, y + 1, w - 2, h - 2, ' ');
     for (i = 0; i != m->vis_start && item; item = item->next, i++) ;
-fprintf(stderr, ">>>%d\n", i);
     if (item) {
 	for (i = 0, j = y + 1; j < y + h - 1 && item; item = item->next, j++, i++) {
 	    if (i == m->cur_pos)
@@ -108,4 +97,35 @@ fprintf(stderr, ">>>%d\n", i);
 	    draw_string(x + 2, j, item->text);
 	}
     }
+}
+
+void tui_menu_key(tui_menu *m, uns16 key)
+{
+    switch (key) {
+    case TUI_UP: m->cur_pos--; break;
+    case TUI_DOWN: m->cur_pos++; break;
+    }
+    if (m->cur_pos < 0) {
+	if (m->vis_start > 0)
+	    m->vis_start--;
+	m->cur_pos = 0;
+    }
+    if (m->cur_pos > m->h - 3) {
+	m->cur_pos--;
+	if (m->vis_start + m->cur_pos < m->num_items - 1)
+	    m->vis_start++;
+    }
+}
+
+char *tui_menu_get_item(tui_menu *m)
+{
+    menu_item *item = m->item;
+    int i = 0;
+    while (item) {
+	if (i == m->vis_start + m->cur_pos)
+	    return item->text;
+	item = item->next;
+	i++;
+    }
+    return NULL;
 }
--- trunk/tui.h
+++ trunk/tui.h
@@ -1,11 +1,28 @@
 #ifndef _TUI_H_
 #define _TUI_H_
 
+enum {
+    TUI_UP = 1,
+    TUI_DOWN
+};
+
+typedef struct {
+    char	*text;
+    void	*next;
+} menu_item;
+
+typedef struct {
+    uns16	w;
+    uns16	h;
+    uns16	vis_start;
+    short	cur_pos;
+    uns16	num_items;
+    menu_item	*item;
+} tui_menu;
+
 void draw_string(uns16 x, uns16 y, char *str);
 
 void draw_box(uns16 x, uns16 y, uns16 w, uns16 h, uns8 c);
-
-typedef struct {} tui_menu;
 
 tui_menu *tui_menu_new(uns16 w, uns16 h);
 
@@ -15,5 +32,10 @@ void tui_menu_free(tui_menu *m);
 
 void tui_menu_dump(tui_menu *m);
 
+void tui_menu_draw(tui_menu *m, uns16 x, uns16 y);
+
+void tui_menu_key(tui_menu *m, uns16 key);
+
+char *tui_menu_get_item(tui_menu *m);
 
 #endif
