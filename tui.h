8b9ab3af43cffb63c7a29a4e1ca9d0d292857735
sashz <23256427+pdaxrom@users.noreply.github.com>
2011-06-18 05:48:13 +0000

    menu name

--- tui.h
+++ tui.h
@@ -17,6 +17,7 @@ typedef struct {
     uns16	vis_start;
     short	cur_pos;
     uns16	num_items;
+    char	*text;
     menu_item	*item;
 } tui_menu;
 
@@ -24,7 +25,7 @@ void draw_string(uns16 x, uns16 y, char *str);
 
 void draw_box(uns16 x, uns16 y, uns16 w, uns16 h, uns8 c);
 
-tui_menu *tui_menu_new(uns16 w, uns16 h);
+tui_menu *tui_menu_new(uns16 w, uns16 h, char *name);
 
 int tui_menu_add_item(tui_menu *m, char *text);
 
